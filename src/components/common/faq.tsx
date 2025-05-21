0956c2291b3ae6273636c004838e631c860d4078
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-21 12:24:54 +0900

    feat/faq: add useMemo to enhance performance

---
+++ src/components/common/faq.tsx
@@ -0,0 +1,7 @@
+export default function Faq() {
+  return (
+    <div className='border border-blue-500 p-4 rounded-xl'>
+      <h2>よくある質問</h2>
+    </div>
+  );
+}
---
+++ src/components/common/faqCarousel.tsx
@@ -0,0 +1,14 @@
+import Faq from '@/components/common/faq';
+
+export default function FaqCarousel() {
+  return (
+    <div className='flex gap-2'>
+      <Faq />
+      <Faq />
+      <Faq />
+      <Faq />
+      <Faq />
+      <Faq />
+    </div>
+  );
+}
