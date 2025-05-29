e17d527fc54050ab7f4827b307d288a62bd6e946
LIU CHIAWEI <doublecheap@gmail.com>
2025-05-29 16:19:03 +0900

    add logo.svg, apply accent color from official site

--- src/components/layout/AppSidebar.tsx
+++ src/components/layout/AppSidebar.tsx
@@ -1,15 +1,16 @@
 import { Sidebar, SidebarHeader, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarFooter, SidebarTrigger } from "@/components/ui/sidebar";
+import Image from "next/image";
 import content from '@/data/content.json';
 export default function AppSidebar() {
   return (
     <Sidebar className="bg-sidebar">
       <SidebarHeader className="items-center justify-center w-full py-3">
-        <SidebarTrigger className="ml-2 rounded-full self-start hover:text-white" tooltipText={content.tooltip.sidebarOff}/>
+        <SidebarTrigger className="ml-2 rounded self-start hover:text-white" tooltipText={content.tooltip.sidebarOff}/>
         <h1 className="text-white md:text-foreground text-shadow-lg md:text-shadow-none text-5xl font-bold tracking-[1.5rem] [writing-mode:vertical-rl] select-none">
           {content.sidebar.header.title}
         </h1>
       </SidebarHeader>
-      <SidebarContent className="mx-0 md:mx-4 border-t">
+      <SidebarContent className="h-full mx-0 md:mx-4 border-t">
         <SidebarGroup>
           <SidebarGroupLabel>
             {content.sidebar.menu.title}
@@ -24,8 +25,14 @@ export default function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        Footer
+      <SidebarFooter className="justify-center items-center px-4">
+        <Image 
+          src="/logo.svg" 
+          alt="logo" 
+          width={140} 
+          height={100} 
+          className="object-contain dark:invert" 
+        />
       </SidebarFooter>
     </Sidebar>
   );
--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -1,15 +1,23 @@
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import NavOptionBar from '@/components/layout/NavOpionBar';
 import content from '@/data/content.json';
+import Image from 'next/image';
 
 export default function Nav({ textScale, onTextScaleChange, style, onStyleChange }: { textScale: string, onTextScaleChange: (scale: string) => void, style: string, onStyleChange: (style: string) => void }) {
   return (
     <nav className='flex items-center justify-between w-full px-4'>
-      <SidebarTrigger className='rounded-full hover:text-white' tooltipText={content.tooltip.sidebarOn}/>
-      <div className='w-full mx-6 md:mx-36 border-l border-r'>
-        <h1 className='py-3 text-center text-lg md:text-xl font-bold tracking-wide md:tracking-[1.5rem] select-none'>
+      <SidebarTrigger className='rounded hover:text-white' tooltipText={content.tooltip.sidebarOn}/>
+      <div className='w-full mx-6 py-2 md:mx-36 border-l border-r'>
+        <Image 
+          src="/logo.svg" 
+          alt="logo" 
+          width={140} 
+          height={100} 
+          className="object-contain dark:invert mx-auto" 
+        />
+        {/* <h1 className='py-3 text-center text-lg md:text-xl font-bold tracking-wide md:tracking-[1.5rem] select-none'>
           {content.navigation.title}
-        </h1>
+        </h1> */}
       </div>
       <NavOptionBar textScale={textScale} onTextScaleChange={onTextScaleChange} style={style} onStyleChange={onStyleChange} />
     </nav>
